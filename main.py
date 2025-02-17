6dea41c57bdaf0dda5ff2f1cda6a4fc0039897bf
Q-bh <kevinknguyen98@yahoo.com>
2025-02-16 19:01:19 -0800

    Reorganize libraries

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import torch, torch.nn as nn
 from sklearn.preprocessing import StandardScaler
+import torch, torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
