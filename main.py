c84d07e5a514f4702a7f0e0320263828a7c5cd81
Q-bh <kevinknguyen98@yahoo.com>
2025-02-16 18:49:37 -0800

    Inital commit

---
+++ main.py
@@ -0,0 +1,7 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch, torch.nn as nn
+from sklearn.preprocessing import StandardScaler
+from torch.utils.data import Dataset, DataLoader
