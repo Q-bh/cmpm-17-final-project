c13a1bea4e2ca50288ce2f17cc4791442d47a35c
Q-bh <kevinknguyen98@yahoo.com>
2025-02-23 18:59:32 -0800

    Add datasets and dataloaders

--- main.py
+++ main.py
@@ -2,6 +2,22 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from PIL import Image
 from sklearn.preprocessing import StandardScaler
 import torch, torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
+import torchvision.datasets as datasets
+from torchvision.transforms import v2
+
+transform = v2.Compose([
+    v2.ToTensor()
+])
+
+train_dataset = datasets.ImageFolder(root="dataset/train", transform=transform)
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True,)
+
+test_dataset = datasets.ImageFolder(root="dataset/test", transform=transform)
+test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
+
+for x, y in train_dataloader:
+    print(x.shape, y.shape)
