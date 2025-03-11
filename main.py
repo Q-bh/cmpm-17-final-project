b2f9d8d844bb3ad0b5564af01e537c31bbb7f158
Liamtsy <liamtsy@icloud.com>
2025-03-11 16:00:36 -0700

    Added Batch norm and dropout

--- main.py
+++ main.py
@@ -33,17 +33,22 @@ class CNN_Main(nn.Module):
         # Two convolutional layers to avoid overfitting.
         # More layers can be added depending on the performance.
 
-        # First convolution:    
+        # First convolution:
         # Input: (batch_size, 1, 128, 128)
         # conv1: (batch_size, 16, 128, 128) [kernel_size = 3, padding = 1]
         self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 16, kernel_size = 3, padding = 1)
+        self.bn1 = nn.BatchNorm2d(16) # Batch normalization, output channel is 16
         self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2) # Make the image as a half size (64)
 
         # Second convolution: Input channel = 16 -> Output Channel = 32
         # Input: (batch_size, 16, 64, 64)
         # conv2: (batch_size, 32, 64, 64) [kernel_size = 3, padding = 1]
         self.conv2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 3, padding = 1)
+        self.bn2 = nn.BatchNorm2d(32) # Batch normalization, output channel is 32
         self.relu = nn.ReLU() # Adding a non-linearity
+
+        # Dropout layer
+        self.dropout = nn.Dropout(0.5)
     
         # After two poolings, image size is 128 -> 64 -> 32
         # Final feature map: (batch_size, 32, 32, 32)
@@ -57,10 +62,12 @@ class CNN_Main(nn.Module):
         # The original data is 48*48, but we transform the image size to 128*128
 
         x = self.conv1(input)
+        x = self.bn1(x)
         x = self.relu(x)
         x = self.pool(x)
         
         x = self.conv2(x)
+        x = self.bn2(x)
         x = self.relu(x)
         x = self.pool(x)
 
@@ -68,6 +75,7 @@ class CNN_Main(nn.Module):
 
         x = self.fc1(x)
         x = self.relu(x)
+        x = self.dropout(x)
         x = self.fc2(x)
 
         return x
@@ -120,7 +128,6 @@ model = CNN_Main()
 loss_function = nn.CrossEntropyLoss()
 
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001) 
-
 
 NUM_EPOCHS = 3
 
@@ -213,3 +220,5 @@ for epoch in range(NUM_EPOCHS):
 # accuracy_test = correct_test / total_test
 
 # print(f"Test Set - Average Loss: {avg_loss_test:.4f}, Accuracy: {accuracy_test:.4f}")
+
+torch.save() ?
