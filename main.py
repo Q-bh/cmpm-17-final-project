4d300501d409586542d8eedc5ce263393504e5c3
Liamtsy <liamtsy@icloud.com>
2025-03-13 16:01:26 -0700

    Putting Validation in epoch for loop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Excess libraries are included in case they are needed
 # They will be removed by the final project's completion if they remain unused
+import time
+import random
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
@@ -11,8 +13,6 @@ from torch.utils.data import Dataset, DataLoader, Subset
 import torchvision.datasets as datasets
 from torchvision.transforms import v2
 import wandb
-import time
-import random
 
 # CLASSES
 
@@ -30,7 +30,9 @@ class CNN_Dataset(Dataset):
 
 # Linear Neural Network
 class CNN_Main(nn.Module):
-    def __init__(self, num_classes = 6):
+    def __init__(self, num_classes = 6, dropout_rate=0.5, conv1_channels=16, conv2_channels=32,
+                 fc1_units=128, conv1_kernel_size = 3, conv1_padding_size = 1, activation=nn.ReLU(),
+                 pool_kernel_size=2, pool_stride_size=2, conv2_kernel_size = 3, conv2_padding_size = 1):
         super().__init__()
 
         # Two convolutional layers to avoid overfitting.
@@ -39,26 +41,26 @@ class CNN_Main(nn.Module):
         # First convolution:
         # Input: (batch_size, 1, 128, 128)
         # conv1: (batch_size, 16, 128, 128) [kernel_size = 3, padding = 1]
-        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = 16, kernel_size = 3, padding = 1)
-        self.bn1 = nn.BatchNorm2d(16) # Batch normalization, output channel is 16
-        self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2) # Make the image as a half size (64)
+        self.conv1 = nn.Conv2d(in_channels = 1, out_channels = conv1_channels, kernel_size = conv1_kernel_size, padding = conv1_padding_size)
+        self.bn1 = nn.BatchNorm2d(conv1_channels) # Batch normalization, output channel is 16
+        self.pool = nn.MaxPool2d(kernel_size = pool_kernel_size, stride = pool_stride_size) # Make the image as a half size (64)
 
         # Second convolution: Input channel = 16 -> Output Channel = 32
         # Input: (batch_size, 16, 64, 64)
         # conv2: (batch_size, 32, 64, 64) [kernel_size = 3, padding = 1]
-        self.conv2 = nn.Conv2d(in_channels = 16, out_channels = 32, kernel_size = 3, padding = 1)
+        self.conv2 = nn.Conv2d(in_channels = conv1_channels, out_channels = conv2_channels, kernel_size = conv2_kernel_size, padding = conv2_padding_size)
         self.bn2 = nn.BatchNorm2d(32) # Batch normalization, output channel is 32
-        self.relu = nn.ReLU() # Adding a non-linearity
+        self.relu = activation # Adding a non-linearity
 
         # Dropout layer
-        self.dropout = nn.Dropout(0.5)
+        self.dropout = nn.Dropout(dropout_rate)
     
         # After two poolings, image size is 128 -> 64 -> 32
         # Final feature map: (batch_size, 32, 32, 32)
         # Flatten = 32 * 32 * 32 = 32786 dimensions
 
-        self.fc1 = nn.Linear(32 * 32 * 32, 128)
-        self.fc2 = nn.Linear(128, num_classes)
+        self.fc1 = nn.Linear(conv2_channels * 32 * 32, fc1_units) # 32, 32 are when pool kernel = 2, pool stride = 2
+        self.fc2 = nn.Linear(fc1_units, num_classes)
 
     def forward(self, input):
         # Input: (batch_size, 1, 128, 128)
@@ -93,7 +95,7 @@ def main():
         print('CUDA is not available. Using CPU.')
 
     # wandb initialization: Saving the name of run and project
-    run = wandb.init(project="CMPM17_FINAL", name="March 12")
+    run = wandb.init(project="CMPM17_FINAL", name="March 13")
 
     # IMAGE TRANSFORMATIONS - Increases model robustness
     train_transforms = v2.Compose([
@@ -134,7 +136,7 @@ def main():
     test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=True)
 
     # INITIALIZATIONS
-    model = CNN_Main().to("cuda")
+    model = CNN_Main().to(device)
 
     # HOW TO READ CROSS ENTROPY LOSS:
     # For an n-class problem, randomly guessing should create an expected loss of -ln(1/n)
@@ -145,7 +147,7 @@ def main():
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001) ###
 
     ###
-    NUM_EPOCHS = 100
+    NUM_EPOCHS = 30
 
     # TRAINING LOOP
     for epoch in range(NUM_EPOCHS):
@@ -182,49 +184,53 @@ def main():
             optimizer.zero_grad() # Resets optimizer to be ready for next epoch
 
             # Saving learning metrics in each batches on wandb
-            run.log({"batch_loss": loss.item()})
+            run.log({"train_batch_losses": loss.item()})
 
             # SCORE - Higher number = Worse performance, it's per one batch
             print(f"Batch Loss: {loss.item():.4f}")
             print(f"Predicted: {predictions}")
             print(f"Confidences: {max_confidences}\n")
         
-    # Average loss and accuracy calculation after one epoch
+        # VALIDATION LOOP
+        model.eval()
+        val_loss = 0.0
+        val_correct_pred = 0
+        val_total_pred = 0
+
+        with torch.no_grad():
+            for image, label in validation_dataloader:
+                
+                image = image.to("cuda")
+                label = label.to("cuda")
+
+                # PREDICT
+                pred = model(image)
+                # SCORE
+                loss = loss_function(pred, label)
+                val_loss += loss.item() * image.size(0)
+                val_total_pred += label.size(0)
+                
+                confidences = torch.softmax(pred, dim=1)
+                max_confidences, predictions = torch.max(confidences, dim=1)
+                val_correct_pred += (predictions == label).sum().item()
+
+                # Saving learning metrics in each batches on wandb
+                run.log({"val_batch_losses": loss.item()})
+        
+    # Average loss and accuracy calculation after one epoch on training
     avg_loss_epoch = total_loss_epoch / total_pred
-    accuracy_epoch = correct_pred / total_pred
-    print(f"Epoch {epoch+1} - Average Loss: {avg_loss_epoch:.4f}, Accuracy: {accuracy_epoch:.4f}\n")
+    avg_accuracy_epoch = correct_pred / total_pred
+    print(f"Epoch {epoch+1} - Average Loss: {avg_loss_epoch:.4f}, Accuracy: {avg_accuracy_epoch:.4f}\n")
 
     # Saving learning metrics in each epoches on wandb
-    run.log({"epoch": epoch+1, "train_loss": avg_loss_epoch, "train_accuracy": accuracy_epoch})
+    run.log({"epoch": epoch+1, "train_loss": avg_loss_epoch, "avg_train_accuracy": avg_accuracy_epoch})
 
-    # VALIDATION LOOP
-    model.eval()
-    val_loss_epoch = 0.0
-    val_correct_pred = 0
-    val_total_pred = 0
-
-    with torch.no_grad():
-        for image, label in validation_dataloader:
-            
-            image = image.to("cuda")
-            label = label.to("cuda")
-
-            # PREDICT
-            pred = model(image)
-            # SCORE
-            loss = loss_function(pred, label)
-            val_loss_epoch += loss.item() * image.size(0)
-            val_total_pred += label.size(0)
-            
-            confidences = torch.softmax(pred, dim=1)
-            max_confidences, predictions = torch.max(confidences, dim=1)
-            val_correct_pred += (predictions == label).sum().item()
-
-    avg_val_loss = val_loss_epoch / val_total_pred
+    # Average loss and accuracy calculation after one epoch on validation
+    avg_val_loss = val_loss / val_total_pred
     accuracy_val = val_correct_pred / val_total_pred
     print(f"Epoch {epoch+1} - Validation Average Loss: {avg_val_loss:.4f}, Accuracy: {accuracy_val:.4f}\n")
 
-    # Saving validation metrics  on wandb
+    # Saving validation metrics on wandb
     run.log({"epoch": epoch+1, "val_loss": avg_val_loss, "val_accuracy": accuracy_val})
     
     # TESTING LOOP
@@ -250,6 +256,9 @@ def main():
     #         confidences = torch.softmax(pred, dim = 1)
     #         max_confidences, predictions = torch.max(confidences, dim = 1)
     #         correct_test += (predictions == label).sum().item()
+    
+    #         Saving learning metrics in each batches on wandb
+    #         run.log({"test_batch_loss": loss.item()})
 
     # avg_loss_test = total_loss_test / total_test
     # accuracy_test = correct_test / total_test
