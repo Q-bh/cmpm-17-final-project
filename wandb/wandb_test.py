349e4c3a1bd036f356d0b0ee55eb015c31f811d3
Liam Youm <83207838+Liamtsy@users.noreply.github.com>
2025-03-18 12:57:16 -0700

    Delete wandb directory

--- wandb/wandb_test.py
+++
@@ -1,9 +0,0 @@
-import wandb
-import time
-import random
-
-run = wandb.init(project = "example-test", name = "my-run")
-
-for i in range(100):
-    run.log({"train loss": i, "validation loss": i + random.random()})
-    time.sleep(0.1)
